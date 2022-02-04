df66d923e0ab424ea28d2e7e4019c672f796c679
anuragkumar151 <63502826+anuragkumar151@users.noreply.github.com>
2022-02-04 18:49:50 +0530

    Update Nav.js

--- Nav.js
+++ Nav.js
@@ -4,25 +4,38 @@ import "./Nav.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 
   function Navbar(){
     return (
-      
-        <div>  
-       <Avatar src="/broken-image.jpg" />
-       <div className="Navbar-right">
+      <div className="header">
+      <div className="header-left">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzRceIIBz4GgeNszaN5SupI6p1SJE_Bzgk3Q&usqp=CAU"
+          alt="gmail logo"
+        />
+      </div>
+      <div className="header-middle">
+        <SearchIcon />
+        <input type="text" placeholder="Search mail" />
+      </div>
+      <div className="header-right">
         <IconButton>
           <AppsIcon />
         </IconButton>
-        
-</div>
-</div>  
- 
-
-
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+       <Avatar name="Foo Bar" />
+      
+      </div>
+    </div>
   );
-  }
+}
 
+        
+
+  
 
 export default Navbar;
